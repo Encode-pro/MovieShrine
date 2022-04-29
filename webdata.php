c5f1b9eb8d6a1a512d512da6acd14abfbf22ce2e
legendofphoenix <82327479+legendofphoenix@users.noreply.github.com>
2022-04-29 19:43:23 +0530

    started_working_movie_database

--- webdata.php
+++ webdata.php
@@ -1,6 +1,14 @@
 <?php
 if(isset($_POST['submitdata']))
 {
+    include "db_conn.php";
+    $name = $_POST['name'];
+    $desc = $_POST['desc'];
+
+
+
+    $sql = "INSERT INTO `moviereview`.`review` ( `name`, `number`, `sreview`, `nreview`, `lreview`, `dt`)
+    VALUES ( '$name', '$number', '$sreview', '$nreview', '$lreview', current_timestamp());";
     echo "Hello";
 }
 ?>
